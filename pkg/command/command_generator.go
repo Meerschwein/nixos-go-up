8c348ea20a09db5165e842d542fedbfb1178cc92
Merlin Volkmer <merlin.volkmer@outlook.com>
2022-06-03 17:15:52 +0200

    add nvme kernel module during boot

--- pkg/command/command_generator.go
+++ pkg/command/command_generator.go
@@ -111,7 +111,7 @@ func WriteNixosConfig(conf configuration.Conf) (_ configuration.Conf, cmds []Com
 			fmt.Sprintf(
 				`
 // {
-  boot.initrd.kernelModules = [ "vfat" "nls_cp437" "nls_iso8859-1" "usbhid" ];
+  boot.initrd.kernelModules = [ "nvme" "vfat" "nls_cp437" "nls_iso8859-1" "usbhid" ];
   boot.initrd.luks.yubikeySupport = true;
   boot.initrd.luks.devices = {
     "%s" = {
