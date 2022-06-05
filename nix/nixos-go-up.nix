ac0e6361dc8d6d0b058de433f265573def3ff8f1
Merlin Volkmer <merlin.volkmer@outlook.com>
2022-06-05 16:48:03 +0200

    remove pbdkf2 dependency

--- nix/nixos-go-up.nix
+++ nix/nixos-go-up.nix
@@ -3,32 +3,26 @@
   makeWrapper,
   lib,
   cryptsetup,
-  openssl,
   parted,
   yubikey-personalization,
   callPackage,
-}: let
-  pbkdf2Sha512 = callPackage ./pbkdf2-sha512.nix {};
-in
-  buildGoModule rec {
-    name = "nixos-go-up";
-    src = ./..;
-    vendorSha256 = "sha256-EbVgItCgIb3Qt22of+Um6bLc8SvqIhp9eXiA/rMoeNU=";
-    doCheck = false;
+}:
+buildGoModule rec {
+  name = "nixos-go-up";
+  src = ./..;
+  vendorSha256 = "sha256-EbVgItCgIb3Qt22of+Um6bLc8SvqIhp9eXiA/rMoeNU=";
+  doCheck = false;
 
-    nativeBuildInputs = [makeWrapper];
+  nativeBuildInputs = [makeWrapper];
 
-    wrapperPath = lib.makeBinPath [
-      cryptsetup
-      openssl
-      parted
-      yubikey-personalization
+  wrapperPath = lib.makeBinPath [
+    cryptsetup
+    parted
+    yubikey-personalization
+  ];
 
-      pbkdf2Sha512
-    ];
-
-    postFixup = ''
-      # Ensure all dependencies are in PATH
-      wrapProgram $out/bin/${name} --prefix PATH : "${wrapperPath}"
-    '';
-  }
+  postFixup = ''
+    # Ensure all dependencies are in PATH
+    wrapProgram $out/bin/${name} --prefix PATH : "${wrapperPath}"
+  '';
+}
